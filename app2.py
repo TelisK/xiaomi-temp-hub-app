aa5baf410b3cb8db1a5281d1901b15beb8b80380
TelisK <tkoutsogiannakis@hotmail.com>
2026-02-26 23:05:04 +0200

    replace print with logging

--- app2.py
+++ app2.py
@@ -9,7 +9,13 @@ from email.mime.text import MIMEText
 import my_accounts
 #db
 import mydb
+#logging
+import logging
 
+FORMAT = '%(asctime)s :  %(levelname)s - %(message)s'
+logging.basicConfig(level=logging.DEBUG, format=FORMAT, handlers=[logging.FileHandler('logs.log'), logging.StreamHandler()])
+
+logger = logging.getLogger(__name__)
 
 
 mydb.db_creation()
@@ -30,7 +36,8 @@ def low_battery_email(battery, name):
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
        smtp_server.login(sender, password)
        smtp_server.sendmail(sender, recipient, msg.as_string())
-    print("Message sent!")
+    #print("Message sent!")
+    logger.info('Low Battery Email Sent.')
     return True
     
 
@@ -43,34 +50,40 @@ def read_data(name:str, device_mac_address):
         # Database
         error_reading = 1 # 1 = True
         mydb.add_to_db(name,now_date_time.strftime('%d-%m-%Y'),now_date_time.strftime('%H:%M:%S'),room_data.temperature,room_data.humidity,room_data.battery, error_reading)
-        print('--------------------')
-        print(f'Rooms Name: {name}\nTemperature: {str(room_data.temperature)}\nHumidity: {str(room_data.humidity)}\nBattery: {str(room_data.battery)}')
-        
+        #print('--------------------')
+        #print(f'Rooms Name: {name}\nTemperature: {str(room_data.temperature)}\nHumidity: {str(room_data.humidity)}\nBattery: {str(room_data.battery)}')
+        logger.info(f'Rooms Name: {name} - Temperature: {str(room_data.temperature)} - Humidity: {str(room_data.humidity)} - Battery: {str(room_data.battery)}')
+
         if room_data.battery <= 25: # Connect with the database so can inform user every -1%.
             previous_batt = mydb.battery_lowest_check(name)
-            print(f'Previous Battery : {previous_batt}')
-            print(f'New Battery : {room_data.battery}')
+
             if previous_batt is not None and previous_batt > room_data.battery:  # compare float with int
                 low_battery_email(room_data.battery, name)
-                print('Low Battery Email Sent')
+                #print('Low Battery Email Sent')
+                logger.warning(f'Low Battery : {room_data.battery}. Sending Email to user.')
                 mydb.update_battery_lowest_update(name, room_data.battery)
             elif previous_batt is None:
                 mydb.update_battery_lowest_update(name, room_data.battery)
-                print('Battery check skipped (No previous data)')
+                #print('Battery check skipped (No previous data)')
+                logger.info('Battery check skipped (No previous data)')
             elif previous_batt - room_data.battery == 0 or previous_batt < room_data.battery:
-                print('Low Battery Email Not Sent - Will sent after 1%% difference')
+                #print('Low Battery Email Not Sent - Will sent after 1% difference')
+                logger.info('Low Battery Email Not Sent - Will sent after 1% difference')
             else:
-                print('Battery check skipped (Something is wrong)')
+                #print('Battery check skipped (Something is wrong)')
+                logger.warning('Battery check skipped (Something is wrong)')
         else:
-            print('Battery is charged')
+            #print('Battery is charged')
+            logger.info('Battery is charged')
 
 
         return str(name), str(room_data.temperature), str(room_data.humidity), str(room_data.battery)
     except Exception as e:
         error_reading = 0 # 0 = False
-        print('--------------------')
-        print(f'Actual Error: {e}') 
-        print(f'Connection Error at room {name}')
+        #print('--------------------')
+        #print(f'Actual Error: {e}') 
+        #print(f'Connection Error at room {name}')
+        logger.error(f'Connection Error at room {name} : {e}')
         return 'Error', {name}
 
 
