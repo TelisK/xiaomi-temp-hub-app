1b770dd53adccd8a894adb16b72a95ba5223d0fa
TelisK <tkoutsogiannakis@hotmail.com>
2026-03-01 17:50:03 +0200

    added timeout. Needs to check if it works correctly

--- app2.py
+++ app2.py
@@ -1,5 +1,5 @@
 from lywsd03mmc import Lywsd03mmcClient
-from datetime import datetime
+from datetime import datetime, timedelta
 import time
 # email
 import smtplib
@@ -11,12 +11,15 @@ import mydb
 import logging
 from dotenv import load_dotenv
 import os
+#timeout
+import signal
 
 #logging config
 FORMAT = '%(asctime)s :  %(levelname)s - %(message)s'
 logging.basicConfig(level=logging.DEBUG, format=FORMAT, handlers=[logging.FileHandler('logs.log'), logging.StreamHandler()])
 
 logger = logging.getLogger(__name__)
+
 
 load_dotenv()
 
@@ -41,7 +44,7 @@ def low_battery_email(battery, name):
             smtp_server.login(sender, password)
             smtp_server.sendmail(sender, recipient, msg.as_string())
 
-        logger.info('Low Battery Email Sent.')
+        logger.warning('Low Battery Email Sent.')
         return True
     except smtplib.SMTPAuthenticationError as e:
         logger.error(f'Email Authentication Failed. Check your Credentials : {e}')
@@ -53,51 +56,56 @@ def low_battery_email(battery, name):
         logger.error(f'Email not sent. Something is wrong: {e}')
         return False
 
-def read_data(name:str, device_mac_address):
+def read_data(name:str, device_mac_address, timeout): # SOS must add timeout 20sec SOS  ALSO CHECK README.md AGAIN
     now_date_time = datetime.now()
-    try:
-        room = Lywsd03mmcClient(device_mac_address)
-        room_data = room.data
-        
-        # Database
-        error_reading = 1 # 1 = True
-        mydb.add_to_db(name,now_date_time.strftime('%d-%m-%Y'),now_date_time.strftime('%H:%M:%S'),room_data.temperature,room_data.humidity,room_data.battery, error_reading)
-        
-        logger.info(f'Rooms Name: {name} - Temperature: {str(room_data.temperature)}°C - Humidity: {str(room_data.humidity)}% - Battery: {str(room_data.battery)}%')
+    function_timeout = now_date_time + timedelta(seconds=timeout)
+    while datetime.now() < function_timeout:
 
-        if room_data.battery <= 25: # Connect with the database so can inform user every -1%.
-            previous_batt = mydb.battery_lowest_check(name)
+        try:
+            room = Lywsd03mmcClient(device_mac_address)
+            room_data = room.data
+            
+            # Database
+            error_reading = 1 # 1 = True
+            mydb.add_to_db(name,now_date_time.strftime('%d-%m-%Y'),now_date_time.strftime('%H:%M:%S'),room_data.temperature,room_data.humidity,room_data.battery, error_reading)
+            
+            logger.info(f'Rooms Name: {name} - Temperature: {str(room_data.temperature)}°C - Humidity: {str(room_data.humidity)}% - Battery: {str(room_data.battery)}%')
 
-            if previous_batt is not None and previous_batt > room_data.battery:  # compare float with int
-                low_battery_email(room_data.battery, name)
-                logger.warning(f'Low Battery : {room_data.battery}%. Sending Email to user.')
-                mydb.update_battery_lowest_update(name, room_data.battery)
-                logger.info('Lowest battery cell updated')
-            elif previous_batt is None:
-                mydb.update_battery_lowest_update(name, room_data.battery)
-                logger.info('Battery check skipped (No previous data)')
-            elif previous_batt - room_data.battery == 0 or previous_batt < room_data.battery:
-                logger.info('Low Battery Email Not Sent - Will sent after 1% difference')
+            if room_data.battery <= 25: # Connect with the database so can inform user every -1%.
+                previous_batt = mydb.battery_lowest_check(name)
+
+                if previous_batt is not None and previous_batt > room_data.battery:  # compare float with int
+                    low_battery_email(room_data.battery, name)
+                    logger.warning(f'Low Battery : {room_data.battery}%. Sending Email to user.')
+                    mydb.update_battery_lowest_update(name, room_data.battery)
+                    logger.info('Lowest battery cell updated')
+                elif previous_batt is None:
+                    mydb.update_battery_lowest_update(name, room_data.battery)
+                    logger.info('Battery check skipped (No previous data)')
+                elif previous_batt - room_data.battery == 0 or previous_batt < room_data.battery:
+                    logger.info('Low Battery Email Not Sent - Will sent after 1% difference')
+                else:
+                    logger.warning('Battery check skipped (Something is wrong)')
             else:
-                logger.warning('Battery check skipped (Something is wrong)')
-        else:
-            logger.info(f'Battery is charged')
+                logger.info(f'Battery is charged')
 
 
-        return str(name), str(room_data.temperature), str(room_data.humidity), str(room_data.battery)
-    except Exception as e:
-        error_reading = 0 # 0 = False
-        logger.error(f'Connection Error at room {name} : {e}')
-        return 'Error', {name}
+            return str(name), str(room_data.temperature), str(room_data.humidity), str(room_data.battery)
+        except Exception as e:
+            error_reading = 0 # 0 = False
+            logger.error(f'Connection Error at room {name} : {e}')
+            return 'Error - Connection Error', {name}
 
-
+    else:
+        logger.error(f'Connection Timeout. Room {name} exceeded time of {timeout} seconds')
+        return 'Error - Connection Timeout', {name}
 
 while True:
 
-    kids_bedroom = read_data('Kids',os.getenv('kids_bedroom'))
-    parents_bedroom = read_data('Parents',os.getenv('parents_bedroom'))
-    living_room = read_data('Living Room',os.getenv('living_room'))
-    airbnb = read_data('Airbnb',os.getenv('airbnb'))
+    kids_bedroom = read_data('Kids',os.getenv('kids_bedroom'), timeout=2)
+    parents_bedroom = read_data('Parents',os.getenv('parents_bedroom'), timeout=2)
+    living_room = read_data('Living Room',os.getenv('living_room'), timeout=2)
+    airbnb = read_data('Airbnb',os.getenv('airbnb'), timeout=2)
 
 
     time.sleep(300)
